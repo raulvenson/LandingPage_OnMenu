c7518f2c7b684ea88f3d8a528e1dc893daf09347
Raul Venson <raulvenson@gmail.com>
2019-11-11 00:30:06 -0400

    updates on carousel

--- src/views/Components/Sections/SectionCarousel.js
+++ src/views/Components/Sections/SectionCarousel.js
@@ -4,15 +4,15 @@ import Carousel from "react-slick";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
-import LocationOn from "@material-ui/icons/LocationOn";
+// import LocationOn from "@material-ui/icons/LocationOn";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Card from "components/Card/Card.js";
 
 import image1 from "assets/img/carousel1.png";
-import image2 from "assets/img/bg2.jpg";
-import image3 from "assets/img/bg3.jpg";
+import image2 from "assets/img/carousel2.png";
+// import image3 from "assets/img/bg3.jpg";
 
 import styles from "assets/jss/material-kit-react/views/componentsSections/carouselStyle.js";
 
@@ -38,20 +38,16 @@ export default function SectionCarousel() {
                 <div>
                   <img src={image1} alt="First slide" className="slick-image" />
                 </div>
-                {/* <div>
+                <div>
                   <img
                     src={image2}
                     alt="Second slide"
                     className="slick-image"
                   />
                   <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      Somewhere Beyond, United States
-                    </h4>
                   </div>
                 </div>
-                <div>
+                {/* <div>
                   <img src={image3} alt="Third slide" className="slick-image" />
                   <div className="slick-caption">
                     <h4>
